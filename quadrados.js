11dd9f7bd67441d00d3f89e59fe803fc04c7f9b0
Your Name <rogeriocfj@gmail.com>
2013-05-08 13:53:04 -0300

    v4

--- quadrados.js
+++ quadrados.js
@@ -1,5 +1,14 @@
+Quadrados = new Meteor.Collection("quadrados");
+
 if (Meteor.isClient) {
-  Template.hello.quadrado = {x: 200, y: 50}
+  Template.hello.quadrado = function(){
+    return Quadrados.findOne(Session.get("quadrado"));
+  }
+
+  Meteor.startup(function () {
+    var quadrado = Quadrados.insert({x: 300, y: 120});
+    Session.set("quadrado", quadrado);
+  });
 }
 
 if (Meteor.isServer) {
