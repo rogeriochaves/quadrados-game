db78abf921ccdc1a78f1680c11edba31199c00ad
Your Name <rogeriocfj@gmail.com>
2013-05-08 13:53:46 -0300

    v6

--- quadrados.js
+++ quadrados.js
@@ -6,8 +6,24 @@ if (Meteor.isClient) {
   };
 
   Meteor.startup(function () {
-    var quadrado = Quadrados.insert({x: Math.random() * 1000, y: Math.random() * 500});
+    var quadrado = Quadrados.insert({x: Math.random() * 500, y: Math.random() * 500});
     Session.set("quadrado", quadrado);
+  });
+
+  $(document).keydown(function (e) {
+    var keyCode = e.keyCode || e.which;
+    var arrow = {37: 'left', 38: 'up', 39: 'right', 40: 'down'};
+
+    var key = arrow[keyCode];
+    if(key === 'right'){
+      Quadrados.update(Session.get("quadrado"), {$inc: {x: 5}});
+    }else if(key === 'left'){
+      Quadrados.update(Session.get("quadrado"), {$inc: {x: -5}});
+    }else if(key === 'up'){
+      Quadrados.update(Session.get("quadrado"), {$inc: {y: -5}});
+    }else if(key === 'down'){
+      Quadrados.update(Session.get("quadrado"), {$inc: {y: 5}});
+    }
   });
 }
 
